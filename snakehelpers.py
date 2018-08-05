2e7165e60592be14774dee6e9fd7a9c08684938f
ksinder <kennethsinder@gmail.com>
2018-08-05 11:21:52 -0700

    Optional

--- snakehelpers.py
+++ snakehelpers.py
@@ -4,22 +4,24 @@ snakehelpers: General-purpose Python 3 helper functions by Kenneth Sinder
 (https://github.com/kennethsinder/snakehelpers)
 """
 
-from typing import Any, Callable, Iterable, Union
+from typing import Any, Callable, Iterable, Optional, Union
 
 
 def any_in(A: Iterable[Any], B: Iterable[Any]) -> bool:
     return any(a in B for a in A)
 
+
 def first(L: Iterable[Any],
-        criterion: Union[bool, Callable[[Any], bool], None],
-        default: Any = None,
-        required: bool = False) -> Any:
+          criterion: Optional[Union[bool, Callable[[Any], bool]]],
+          default: Optional[Any] = None,
+          required: Optional[bool] = False) -> Any:
     if criterion is None:
-        pred = lambda x: True
+        def pred(x): return True
     elif isinstance(criterion, bool):
-        pred = lambda x: criterion
+        def pred(x): return criterion
     else:
         pred = criterion
+
     try:
         return next(x for x in L if pred(x))
     except StopIteration:
